033caa7519c49cfb2457e2361791e9cbfb3f7c90
Gajda <staniopl99@gmail.com>
2019-07-17 01:18:27 +0200

    Update mobile menu
    Update mobile menu, change design and action

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-document.getElementById("#nav_mobile_menu").addEventListener("click", function() { alert("Hello World!"); });
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById("nav_mobile_menu").addEventListener("click", function() {
+        document.getElementById("div_mobile_menu").style.display = "block";
+        document.getElementById("nav_mobile_menu").style.display = "none";
+    });
+    document.getElementById("mobile_menu_img_close").addEventListener("click", function() {
+        document.getElementById("div_mobile_menu").style.display = "none";
+        document.getElementById("nav_mobile_menu").style.display = "block";
+    });
+    let menu_mobile_link = document.getElementsByClassName("menu_mobile_link");
+    for (let i = 0; i < menu_mobile_link.length; i++) {
+        menu_mobile_link[i].addEventListener("click", function() {
+            document.getElementById("div_mobile_menu").style.display = "none";
+            document.getElementById("nav_mobile_menu").style.display = "block";
+        });
+    }
+});
