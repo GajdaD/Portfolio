90f0d6d2233f5eafc341e6d365a09436685c6950
Gajda <staniopl99@gmail.com>
2019-07-17 04:54:33 +0200

    Add animation for mobile menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,57 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    document.getElementById("nav_mobile_menu").addEventListener("click", function() {
+    document.getElementById("nav_mobile_menu").addEventListener("click", animation_function)
+
+    function animation_function() {
+        document.getElementById("nav_mobile_menu").removeEventListener("click", animation_function);
+        let i_1 = 0
+        let i_2 = 0;
+        let animation_1 = setInterval(f_animation_1, 5);
+        document.getElementById("animation_mobile_menu").style.display = "block";
         document.getElementById("div_mobile_menu").style.display = "block";
         document.getElementById("nav_mobile_menu").style.display = "none";
-    });
-    document.getElementById("mobile_menu_img_close").addEventListener("click", function() {
-        document.getElementById("div_mobile_menu").style.display = "none";
-        document.getElementById("nav_mobile_menu").style.display = "block";
-    });
+
+        function f_animation_1() {
+            i_1++;
+
+            document.getElementById("animation_mobile_menu").style.width = i_1 + "%";
+            document.getElementById("animation_mobile_menu").style.height = i_1 + "%";
+            if (i_1 > 50) {
+                i_2 += 0.02;
+                document.getElementById("animation_mobile_menu").style.opacity = (1.5 - i_2);
+                document.getElementById("div_mobile_menu").style.opacity = i_2;
+            }
+            if (i_1 > 100) {
+                clearInterval(animation_1);
+                document.getElementById("animation_mobile_menu").style.display = "none";
+                document.getElementById("mobile_menu_img_close").addEventListener("click", animation_function_2)
+            }
+        }
+
+    };
+
+    function animation_function_2() {
+        document.getElementById("mobile_menu_img_close").removeEventListener("click", animation_function_2);
+        let i_1 = 100;
+        let i_2 = 1;
+        let animation = setInterval(f_animation, 5);
+        document.getElementById("animation_mobile_menu_out").style.display = "block";
+
+        function f_animation() {
+            i_1--;
+            i_2 -= 0.01;
+
+            document.getElementById("animation_mobile_menu_out").style.width = i_1 + "%";
+            document.getElementById("animation_mobile_menu_out").style.height = i_1 + "%";
+            document.getElementById("div_mobile_menu").style.opacity = i_2;
+            if (i_1 <= 0) {
+                clearInterval(animation);
+                document.getElementById("div_mobile_menu").style.display = "none";
+                document.getElementById("nav_mobile_menu").style.display = "block";
+                document.getElementById("animation_mobile_menu_out").style.display = "none";
+                document.getElementById("nav_mobile_menu").addEventListener("click", animation_function)
+            }
+        }
+    };
     let menu_mobile_link = document.getElementsByClassName("menu_mobile_link");
     for (let i = 0; i < menu_mobile_link.length; i++) {
         menu_mobile_link[i].addEventListener("click", function() {
@@ -15,3 +60,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
         });
     }
 });
+//https://icons8.com/icons
+//https://www.w3schools.com/css/css3_gradients.asp
+//https://www.rafalrebacz.pl/2016/06/10/shadow-cien-w-css/
+//https://cssanimation.rocks/pl/portal/
+//https://www.w3schools.com/css/css3_animations.asp
