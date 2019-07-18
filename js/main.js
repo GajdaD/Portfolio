020a6b2e8aadb29ef9d7e18ae95e2fb70b127545
Gajda <staniopl99@gmail.com>
2019-07-18 20:37:34 +0200

    Add navbar action while scroll window

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+    // Mobile menu pop out
     document.getElementById("nav_mobile_menu").addEventListener("click", animation_function)
 
     function animation_function() {
@@ -25,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
             }
         }
     };
-
+    // Mobile menu hide away - close button
     function animation_function_2() {
         document.getElementById("mobile_menu_img_close").removeEventListener("click", animation_function_2);
         let i_1 = 100;
@@ -42,7 +43,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 if (flag == true) {
                     document.getElementById("animation_mobile_menu").style.display = "none";
                     flag = false;
-                    console.log(1)
                 }
 
                 i_1--;
@@ -54,15 +54,59 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 document.getElementById("div_mobile_menu").style.display = "none";
                 document.getElementById("nav_mobile_menu").style.display = "block";
                 document.getElementById("animation_mobile_menu_out").style.display = "none";
-                document.getElementById("nav_mobile_menu").addEventListener("click", animation_function)
+                document.getElementById("nav_mobile_menu").addEventListener("click", animation_function);
             }
         }
     };
+    // Mobile menu hide away - click link
     let menu_mobile_link = document.getElementsByClassName("menu_mobile_link");
     for (let i = 0; i < menu_mobile_link.length; i++) {
         menu_mobile_link[i].addEventListener("click", function() {
+            document.getElementById("animation_mobile_menu").style.display = "none";
             document.getElementById("div_mobile_menu").style.display = "none";
             document.getElementById("nav_mobile_menu").style.display = "block";
+            document.getElementById("nav_mobile_menu").addEventListener("click", animation_function);
+            document.getElementById("div_mobile_menu").style.opacity = 0;
         });
     }
+    // Scrolling window
+    var scroll_stop = function(callback) {
+
+        if (!callback || typeof callback !== 'function') return;
+
+        var is_scrolling;
+        let elem = document.getElementById("nav_mobile_center").style.width = 100 + "%";
+        let elem_width = elem.slice(0, -1);
+
+        window.addEventListener('scroll', function(event) {
+            elem = document.getElementById("nav_mobile_center").style.width;
+            elem_width = elem.slice(0, -1);
+            if (elem_width > 70) {
+                elem_width--;
+                document.getElementById("nav_mobile_center").style.width = elem_width + "%";
+            }
+
+            window.clearTimeout(is_scrolling);
+            is_scrolling = setTimeout(function() {
+                callback();
+            }, 66);
+        }, false);
+    };
+    scroll_stop(function() {
+        let animation = setInterval(f_animation, 10);
+
+        let elem_2 = document.getElementById("nav_mobile_center").style.width;
+        let elem_width_2 = elem_2.slice(0, -1);
+
+        function f_animation() {
+            elem_2 = document.getElementById("nav_mobile_center").style.width;
+            elem_width_2 = elem_2.slice(0, -1);
+            if (elem_width_2 < 100) {
+                elem_width_2++;
+                document.getElementById("nav_mobile_center").style.width = elem_width_2 + "%";
+            } else {
+                clearInterval(animation)
+            }
+        }
+    });
 });
