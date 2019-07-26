768116e660647d5833cb44b74099445ce5108e1d
Gajda <staniopl99@gmail.com>
2019-07-26 19:42:49 +0200

    Update desgin, fix mobile menu

--- js/main.js
+++ js/main.js
@@ -6,17 +6,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
         document.getElementById("nav_mobile_menu").removeEventListener("click", animation_function);
         let i_1 = 0
         let i_2 = 0;
-        let animation_1 = setInterval(f_animation_1, 4);
+        let animation_1 = setInterval(f_animation_1, 5);
         document.getElementById("animation_mobile_menu").style.display = "block";
         document.getElementById("div_mobile_menu").style.display = "block";
         document.getElementById("nav_mobile_menu").style.display = "none";
 
         function f_animation_1() {
-            i_1++;
+            i_1 += 2;
             document.getElementById("animation_mobile_menu").style.width = i_1 + "%";
             document.getElementById("animation_mobile_menu").style.height = i_1 + "%";
             if (i_1 >= 100) {
-                i_2 += 0.02;
+                i_2 += 0.04;
                 document.getElementById("div_mobile_menu").style.opacity = i_2;
 
             }
@@ -31,13 +31,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
         document.getElementById("mobile_menu_img_close").removeEventListener("click", animation_function_2);
         let i_1 = 100;
         let i_2 = 1;
-        let animation = setInterval(f_animation, 4);
+        let animation = setInterval(f_animation, 5);
         let flag = true;
         document.getElementById("animation_mobile_menu_out").style.display = "block";
 
         function f_animation() {
 
-            i_2 -= 0.02;
+            i_2 -= 0.04;
             document.getElementById("div_mobile_menu").style.opacity = i_2;
             if (i_2 <= 0) {
                 if (flag == true) {
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
                     flag = false;
                 }
 
-                i_1--;
+                i_1 -= 2;
                 document.getElementById("animation_mobile_menu_out").style.width = i_1 + "%";
                 document.getElementById("animation_mobile_menu_out").style.height = i_1 + "%";
             }
